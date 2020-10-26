85d0abed768a9915f926d360e532628e1005a426
KertisJ <48452042+KertisJ@users.noreply.github.com>
2020-10-26 19:18:48 +0100

    JM: I/O streams v.1 & Main

---
+++ src/IStream.java
@@ -0,0 +1,181 @@
+import java.io.*;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+/* 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+ */
+
+public interface IStream {
+
+    void open(String f_path) throws IOException;
+    String readln() throws IOException;
+    void seek(long pos) throws IOException;
+    boolean end_of_stream() throws IOException;
+    char EOL = '\n';
+
+
+    public class IStream1 implements IStream {
+        private RandomAccessFile f;                          // better than filereader or fileinputstream
+    
+        public void open(String f_path) throws FileNotFoundException {
+            this.f = new RandomAccessFile(f_path, "r");
+        }
+    
+        public String readln() throws IOException {
+            StringBuilder b = new StringBuilder();
+            char c;
+            do {
+                c = (char) this.f.read();
+                b.append(c);
+            } while (c != EOL);
+            return b.toString();
+        }
+    
+        public void seek(long pos) throws IOException {
+            this.f.seek(pos);
+        }
+    
+        public boolean end_of_stream() throws IOException {
+            long currentPos = this.f.getFilePointer();
+            if (this.f.read() == -1) {                        // the InputStream will signal EOF by returning -1 from read()
+                return true;
+            } else {
+                this.f.seek(currentPos);
+                return false;
+            }
+        }
+    }
+
+
+
+    public class IStream2 implements IStream {
+        private RandomAccessFile f;
+        private BufferedReader br;
+    
+        public void open(String f_path) throws IOException {
+            this.f = new RandomAccessFile(f_path, "r");
+            this.br = new BufferedReader(new FileReader(this.f.getFD()));
+        }
+    
+        public String readln() throws IOException {
+            return br.readLine();
+        }
+    
+        public void seek(long pos) throws IOException {
+            this.f.seek(pos);
+            this.br = new BufferedReader(new FileReader(this.f.getFD()));
+        }
+    
+        public boolean end_of_stream() throws IOException {
+            long currentPos = this.f.getFilePointer();
+            String line = br.readLine();
+            if (line != null) {
+                this.f.seek(currentPos);
+                return false;
+            } else
+                return true;
+        }
+    }
+
+
+
+    public class IStream3 implements IStream {
+        private RandomAccessFile f;
+        private BufferedReader br;
+        private int B;
+    
+        public IStream3(int bufferSize) {
+            this.B = bufferSize;
+        }
+    
+        public void open(String f_path) throws IOException {
+            this.f = new RandomAccessFile(f_path, "r");
+            this.br = new BufferedReader(new FileReader(this.f.getFD()), this.B);
+        }
+    
+        public String readln() throws IOException {         // ** Add the buffer condition
+            StringBuilder b = new StringBuilder();
+            char c;
+            do {
+                c = (char) this.br.read();
+                if (c != EOL)
+                    b.append(c);
+            } while (c != EOL);
+            return b.toString();
+        }
+    
+        public void seek(long pos) throws IOException {
+            this.f.seek(pos);
+            this.br = new BufferedReader(new FileReader(this.f.getFD()));
+        }
+    
+        public boolean end_of_stream() throws IOException {
+            long currentPos = this.f.getFilePointer();
+            String line = br.readLine();
+            if (line != null) {
+                this.f.seek(currentPos);
+                return false;
+            } else
+                return true;
+        }
+    }
+
+
+
+    public class IStream4 implements IStream {
+        private MappedByteBuffer Bb;
+        private RandomAccessFile f;
+        private long B, nextPos;
+    
+        public IStream4(int bufferSize) {
+            this.B = bufferSize;
+        }
+    
+        public void open(String f_path) throws IOException {
+            this.f = new RandomAccessFile(f_path, "r");
+            this.map();
+        }
+    
+        public String readln() {                // https://howtodoinjava.com/java/nio/memory-mapped-files-mappedbytebuffer/
+            StringBuilder b = new StringBuilder();
+            char c;
+            do {
+                c = this.readChar();
+                b.append(c);
+            } while (c != EOL);
+            return b.toString();
+        }
+    
+        public void seek(long pos) throws IOException {
+            this.nextPos = pos;
+            this.map();
+        }
+        
+        public boolean end_of_stream() throws IOException {
+            return !this.Bb.hasRemaining() && this.nextPos >= this.f.length();
+        }
+    
+        private void map() throws IOException {
+            long size = Math.min(this.f.length() - this.nextPos, this.B);
+            this.Bb = this.f.getChannel().map(FileChannel.MapMode.READ_ONLY, this.nextPos, size);
+            this.nextPos += size;
+        }
+    
+        private char readChar() {
+            if (!this.Bb.hasRemaining()) {
+                try {
+                    this.map();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return (char) this.Bb.get();
+        }
+    }
+
+}
+
---
+++ src/OStream.java
@@ -0,0 +1,131 @@
+import java.io.*;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+
+public interface OStream {
+
+    void create(String f_path) throws IOException;
+    void writeln(String string) throws IOException;
+    void close() throws IOException;
+    char EOL = '\n';
+
+
+    public class OStream1 implements OStream {
+        private FileWriter f;
+    
+        public void create(String f_path) throws IOException {  
+            f = new FileWriter(f_path);
+        }
+    
+        public void writeln(String str) throws IOException {    // https://beginnersbook.com/2013/12/java-string-tochararray-method-example/
+            for (char c : str.toCharArray()) {                //converting c into sequence of characters and return an Array of chars
+                f.write(c);
+            }
+            f.write(EOL);
+        }
+        
+        public void close() throws IOException {
+            f.close();
+        }
+    }
+
+
+
+    public class OStream2 implements OStream {
+        private BufferedWriter bw;
+    
+        public void create(String f_path) throws IOException {  // https://www.programcreek.com/2011/03/fileoutputstream-vs-filewriter/
+            this.bw = new BufferedWriter(new FileWriter(f_path));
+        }
+    
+        public void writeln(String str) throws IOException {
+            this.bw.write(str);
+            this.bw.newLine();
+        }
+    
+        public void close() throws IOException {
+            this.bw.close();
+        }
+    }
+
+
+
+    public class OStream3 implements OStream {
+        private BufferedWriter bw;
+        private int B;
+    
+        public OStream3(int bufferSize) {
+            this.B = bufferSize;
+        }
+    
+        public void create(String f_path) throws IOException {
+            bw = new BufferedWriter(new FileWriter(f_path), B);
+        }
+    
+        public void writeln(String str) throws IOException {    // ** Add the buffer condition
+            for (char ch : str.toCharArray()) {
+                bw.write(ch);
+            }
+            bw.newLine();
+        }
+    
+        public void close() throws IOException {
+            bw.close();
+        }
+    }
+
+
+
+    public class OStream4 implements OStream {
+        private MappedByteBuffer Bb;
+        private RandomAccessFile f;
+        private FileChannel fChannel;
+        private int B;
+        private int bCount;
+    
+        public OStream4(int bufferSize) {
+            this.B = bufferSize;
+        }
+    
+        public void create(String f_path) throws IOException {      // https://howtodoinjava.com/java/nio/memory-mapped-files-mappedbytebuffer/
+            this.f = new RandomAccessFile(f_path, "rw");
+            this.bCount = 0;
+            this.map();
+        }
+    
+        public void writeln(String line) {
+            for (char c : line.toCharArray()) {
+                putChar(c);
+            }
+            putChar(EOL);
+        }
+    
+        public void close() throws IOException {
+            this.Bb.force();
+            this.fChannel.truncate(this.bCount * this.B - this.Bb.remaining());
+            this.fChannel.close();
+            this.Bb.clear();
+            this.f.close();
+        }
+    
+        private void map() throws IOException {
+            this.f.setLength(0);
+            this.fChannel = this.f.getChannel();
+            this.Bb = this.fChannel.map(FileChannel.MapMode.READ_WRITE, this.bCount * this.B, this.B);
+            this.bCount += 1;
+        }
+    
+        private void putChar(char c) {
+            if (!this.Bb.hasRemaining()) {
+                try {
+                    this.map();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            this.Bb.put((byte) c);
+        }
+    }
+
+}
+
